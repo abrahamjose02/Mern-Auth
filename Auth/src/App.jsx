6fd1ddea77060af18222d528696352056a49c7bc
Abraham Jose <abrahamjose002@gmail.com>
2024-05-18 10:05:49 +0530

    Install React js and Tailwind CSS

---
+++ Auth/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, Tailwind CSS with React and Vite!
+      </h1>
+    </div>
+  )
+}
+
+export default App
