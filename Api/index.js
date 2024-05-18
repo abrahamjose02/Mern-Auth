0f662ce39ee0f208450c316aaaa2c82422ec6c6e
Abraham Jose <abrahamjose002@gmail.com>
2024-05-18 12:40:04 +0530

    connect to db

---
+++ Api/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error } from "console";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(() => {
+    console.log(error);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server listening on Port 3000 !");
+});
