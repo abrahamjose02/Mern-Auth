b10fccf2e20b66e782a01a722f873bf49e27a2f9
Abraham Jose <abrahamjose002@gmail.com>
2024-05-21 06:52:01 +0530

    upload image functionality

--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -49,7 +49,7 @@ function Profile() {
           onChange={(e) => setImage(e.target.files[0])}
         />
         <img
-          src={currentUser.profilePicture}
+          src={formData.profilePicture || currentUser.profilePicture}
           alt="profile"
           className="h-24 w-24 self-center cursor-pointer rounded-full object-cover"
           onClick={() => fileRef.current.click()}
